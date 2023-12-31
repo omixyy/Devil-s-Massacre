9be659fb23f38ce0fd1a3f89b3473f547c42846e
Ivan Kaziev <ivkaziev@yandex.ru>
2023-12-31 17:59:43 +0300

    More decorations added, player can move by using mouse now

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import pytmx
 import json
 
 # Константы
-FPS = 50
+FPS = 60
 TORCHES_DIR = 'tiles/2D Pixel Dungeon Asset Pack/items and trap_animation/torch'
 COINS_DIR = 'tiles/2D Pixel Dungeon Asset Pack/items and trap_animation/coin'
 CHESTS_DIR = 'tiles/2D Pixel Dungeon Asset Pack/items and trap_animation/chest'
 PLAYERS_DIR = 'tiles/2D Pixel Dungeon Asset Pack/Character_animation/priests_idle/priest3/v2'
+POINTER_DIR = 'tiles/2D Pixel Dungeon Asset Pack/interface'
 SPRITE_SIZE = 16
-PLAYER_SPEED = 3
+PLAYER_SPEED = 120 / FPS
 
 # Считываем координаты для анимированных декораций из json
 with open('maps/level1/elements_pos.json', 'r', encoding='utf8') as jsonf:
@@ -22,7 +23,7 @@ coins = pg.sprite.Group()
 animated_sprites = pg.sprite.Group()
 
 
-def terminate():
+def terminate() -> None:
     pg.quit()
     sys.exit()
 
@@ -30,6 +31,7 @@ def terminate():
 class AnimatedObject(pg.sprite.Sprite):
     def __init__(self, group: list, directory: str, x: int, y: int, filename: str) -> None:
         super().__init__(*group)
+        self.filename = filename
         self.pos = x, y
         self.flip = False
         self.images = [directory + f'/{filename}_{i}.png' for i in range(1, 5)]
@@ -72,22 +74,34 @@ class Player(AnimatedObject):
     def __init__(self, x: int, y: int, filename: str) -> None:
         super().__init__([animated_sprites], PLAYERS_DIR, x, y, filename)
 
-    def move(self, dx=1, dy=1) -> None:
+    def move_by_delta(self, dx=1.0, dy=1.0) -> None:
         self.pos = self.pos[0] + dx, self.pos[1] + dy
         self.rect.x, self.rect.y = self.pos[0], self.pos[1]
         screen.blit(self.image, self.pos)
 
     def get_left_up_cell(self) -> tuple[int, int]:
-        return self.pos[0] // SPRITE_SIZE, self.pos[1] // SPRITE_SIZE
+        return int(self.pos[0] // SPRITE_SIZE), int(self.pos[1] // SPRITE_SIZE)
 
     def get_left_down_cell(self) -> tuple[int, int]:
-        return self.pos[0] // SPRITE_SIZE, self.pos[1] // SPRITE_SIZE + 1
+        return int(self.pos[0] // SPRITE_SIZE), int(self.pos[1] // SPRITE_SIZE + 1)
 
     def get_right_up_cell(self) -> tuple[int, int]:
-        return self.pos[0] // SPRITE_SIZE + 1, self.pos[1] // SPRITE_SIZE
+        return int(self.pos[0] // SPRITE_SIZE + 1), int(self.pos[1] // SPRITE_SIZE)
 
     def get_right_down_cell(self) -> tuple[int, int]:
-        return self.pos[0] // SPRITE_SIZE + 1, self.pos[1] // SPRITE_SIZE + 1
+        return int(self.pos[0] // SPRITE_SIZE + 1), int(self.pos[1] // SPRITE_SIZE + 1)
+
+    def get_center_cell(self) -> tuple[int, int]:
+        return (int((self.pos[0] + SPRITE_SIZE // 2) // SPRITE_SIZE),
+                int((self.pos[1] + SPRITE_SIZE // 2) // SPRITE_SIZE))
+
+    def get_center_coordinates(self) -> tuple[float, float]:
+        return self.pos[0] + SPRITE_SIZE / 2, self.pos[1] + SPRITE_SIZE / 2
+
+
+class Pointer(AnimatedObject):
+    def __init__(self, x: int, y: int, filename) -> None:
+        super().__init__([animated_sprites, decorative], POINTER_DIR, x, y, filename)
 
 
 class Castle:
@@ -108,6 +122,29 @@ class Castle:
                 if decoration_image is not None:
                     screen.blit(decoration_image, (x * SPRITE_SIZE, y * SPRITE_SIZE))
 
+    def find_path_step(self, start, target):
+        inf = 1000
+        x, y = start
+        distance = [[inf] * self.width for _ in range(self.height)]
+        distance[y][x] = 0
+        prev = [[None] * self.width for _ in range(self.height)]
+        queue = [(x, y)]
+        while queue:
+            x, y = queue.pop(0)
+            for dx, dy in (1, 0), (0, 1), (-1, 0), (0, -1):
+                next_x, next_y = x + dx, y + dy
+                if 0 <= next_x <= self.width and 0 <= next_y <= self.height and \
+                        self.is_free((next_x, next_y)) and distance[next_y][next_x] == inf:
+                    distance[next_y][next_x] = distance[y][x] + 1
+                    prev[next_y][next_x] = (x, y)
+                    queue.append((next_x, next_y))
+        x, y = target
+        if distance[y][x] == inf or start == target:
+            return start
+        while prev[y][x] != start:
+            x, y = prev[y][x]
+        return x, y
+
     def get_tile_id(self, position) -> int:
         return self.map.tiledgidmap[self.map.get_tile_gid(*position, layer=0)] - 1
 
@@ -115,15 +152,7 @@ class Castle:
         return self.get_tile_id(position) not in self.walls
 
 
-if __name__ == '__main__':
-    pg.init()
-    pg.display.set_caption("Devil's Massacre")
-    screen = pg.display.set_mode((WIDTH := 800, HEIGHT := 640))
-    screen.fill(pg.Color('black'))
-    castle = Castle('level1', 'level1.tmx')
-    running = True
-    clock = pg.time.Clock()
-    castle.render()
+def add_decor_elements() -> None:
     for elem, crd in coordinates.items():
         for pos in crd:
             pos_x, pos_y = pos[0] * SPRITE_SIZE, pos[1] * SPRITE_SIZE
@@ -135,6 +164,63 @@ if __name__ == '__main__':
                 Coin(pos_x, pos_y, 'coin')
             elif elem == 'big-chests':
                 Chest(pos_x, pos_y, 'chest')
+
+
+def move_by_key(keys: pg.key.ScancodeWrapper) -> None:
+    current_pos_rd = player.get_right_down_cell()
+    current_pos_lu = player.get_left_up_cell()
+    current_pos_ru = player.get_right_up_cell()
+    current_pos_ld = player.get_left_down_cell()
+    if keys[pg.K_s]:
+        if castle.is_free(current_pos_rd) and castle.is_free(current_pos_ld) and \
+                castle.is_free((current_pos_rd[0], (player.pos[1] + PLAYER_SPEED) // SPRITE_SIZE + 1)) and \
+                castle.is_free((current_pos_ld[0], (player.pos[1] + PLAYER_SPEED) // SPRITE_SIZE + 1)):
+            player.move_by_delta(dx=0, dy=PLAYER_SPEED)
+    if keys[pg.K_w]:
+        if castle.is_free(current_pos_ru) and castle.is_free(current_pos_lu) and \
+                castle.is_free((current_pos_ru[0], (player.pos[1] - PLAYER_SPEED) // SPRITE_SIZE)) and \
+                castle.is_free((current_pos_lu[0], (player.pos[1] - PLAYER_SPEED) // SPRITE_SIZE)):
+            player.move_by_delta(dx=0, dy=-PLAYER_SPEED)
+    if keys[pg.K_a]:
+        if castle.is_free(current_pos_lu) and castle.is_free(current_pos_ld) and \
+                castle.is_free(((player.pos[0] - PLAYER_SPEED) // SPRITE_SIZE, current_pos_lu[1])) and \
+                castle.is_free(((player.pos[0] - PLAYER_SPEED) // SPRITE_SIZE, current_pos_ld[1])):
+            player.move_by_delta(dx=-PLAYER_SPEED, dy=0)
+            player.flip = True
+    if keys[pg.K_d]:
+        if castle.is_free(current_pos_ru) and castle.is_free(current_pos_rd) and \
+                castle.is_free(((player.pos[0] + PLAYER_SPEED) // SPRITE_SIZE + 1, current_pos_ru[1])) and \
+                castle.is_free(((player.pos[0] + PLAYER_SPEED) // SPRITE_SIZE + 1, current_pos_rd[1])):
+            player.move_by_delta(dx=PLAYER_SPEED, dy=0)
+            player.flip = False
+
+
+def move_by_pointer(to: tuple[int, int], obj: Player) -> None:
+    current_cell = obj.get_center_cell()
+    next_pos = castle.find_path_step(current_cell, to)
+    delta_x, delta_y = next_pos[0] - current_cell[0], next_pos[1] - current_cell[1]
+    obj.flip = delta_x < 0
+    obj.move_by_delta(dx=delta_x * PLAYER_SPEED, dy=delta_y * PLAYER_SPEED)
+
+
+def kill_arrow() -> None:
+    for obj in animated_sprites:
+        if obj.filename == 'arrow':
+            obj.kill()
+
+
+if __name__ == '__main__':
+    pg.init()
+    pg.display.set_caption("Devil's Massacre")
+    screen = pg.display.set_mode((WIDTH := 800, HEIGHT := 640))
+    screen.fill(pg.Color('black'))
+    castle = Castle('level1', 'level1.tmx')
+    running = True
+    pointed = False
+    move_to = None
+    clock = pg.time.Clock()
+    castle.render()
+    add_decor_elements()
     player = Player(2 * SPRITE_SIZE, 2 * SPRITE_SIZE, 'priest3_v2')
     while running:
         pressed = pg.key.get_pressed()
@@ -142,32 +228,19 @@ if __name__ == '__main__':
             if event.type == pg.QUIT:
                 terminate()
                 running = False
-        current_pos_rd = player.get_right_down_cell()
-        current_pos_lu = player.get_left_up_cell()
-        current_pos_ru = player.get_right_up_cell()
-        current_pos_ld = player.get_left_down_cell()
-        if pressed[pg.K_s]:
-            if castle.is_free(current_pos_rd) and castle.is_free(current_pos_ld) and\
-                    castle.is_free((current_pos_rd[0], (player.pos[1] + PLAYER_SPEED) // SPRITE_SIZE + 1)) and\
-                    castle.is_free((current_pos_ld[0], (player.pos[1] + PLAYER_SPEED) // SPRITE_SIZE + 1)):
-                player.move(dx=0, dy=PLAYER_SPEED)
-        if pressed[pg.K_w]:
-            if castle.is_free(current_pos_ru) and castle.is_free(current_pos_lu) and\
-                    castle.is_free((current_pos_ru[0], (player.pos[1] - PLAYER_SPEED) // SPRITE_SIZE)) and\
-                    castle.is_free((current_pos_lu[0], (player.pos[1] - PLAYER_SPEED) // SPRITE_SIZE)):
-                player.move(dx=0, dy=-PLAYER_SPEED)
-        if pressed[pg.K_a]:
-            if castle.is_free(current_pos_lu) and castle.is_free(current_pos_ld) and\
-                    castle.is_free(((player.pos[0] - PLAYER_SPEED) // SPRITE_SIZE, current_pos_lu[1])) and\
-                    castle.is_free(((player.pos[0] - PLAYER_SPEED) // SPRITE_SIZE, current_pos_ld[1])):
-                player.move(dx=-PLAYER_SPEED, dy=0)
-                player.flip = True
-        if pressed[pg.K_d]:
-            if castle.is_free(current_pos_ru) and castle.is_free(current_pos_rd) and \
-                    castle.is_free(((player.pos[0] + PLAYER_SPEED) // SPRITE_SIZE + 1, current_pos_ru[1])) and \
-                    castle.is_free(((player.pos[0] + PLAYER_SPEED) // SPRITE_SIZE + 1, current_pos_rd[1])):
-                player.move(dx=PLAYER_SPEED, dy=0)
-                player.flip = False
+            if event.type == pg.MOUSEBUTTONDOWN:
+                if event.button == 3:
+                    pointed = True
+                    move_to = event.pos[0] // SPRITE_SIZE, event.pos[1] // SPRITE_SIZE
+                    if len([i for i in animated_sprites if i.filename == 'arrow']):
+                        kill_arrow()
+                    Pointer(move_to[0] * SPRITE_SIZE, move_to[1] * SPRITE_SIZE, 'arrow')
+        move_by_key(pressed)
+        if pointed:
+            move_by_pointer(move_to, player)
+        if player.get_center_cell() == move_to:
+            pointed = False
+            kill_arrow()
         castle.render()
         for sprite in animated_sprites:
             sprite.animate()

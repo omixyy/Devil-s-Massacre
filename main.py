800fc66c4e2a9a58621e2bf5afe9cb532c407a6e
Ivan Kaziev <ivkaziev@yandex.ru>
2024-01-03 17:07:09 +0300

    New file for constants

--- main.py
+++ main.py
@@ -3,21 +3,7 @@ from random import choice
 import sys
 import pytmx
 import json
-
-# Константы
-FPS = 60
-TORCHES_DIR = 'tiles/2D Pixel Dungeon Asset Pack/items and trap_animation/torch'
-COINS_DIR = 'tiles/2D Pixel Dungeon Asset Pack/items and trap_animation/coin'
-CHESTS_DIR = 'tiles/2D Pixel Dungeon Asset Pack/items and trap_animation/chest'
-PLAYERS_DIR = 'tiles/2D Pixel Dungeon Asset Pack/Character_animation/priests_idle/priest3/v2'
-INTERFACE_DIR = 'tiles/2D Pixel Dungeon Asset Pack/interface'
-FLASKS_DIR = 'tiles/2D Pixel Dungeon Asset Pack/items and trap_animation/flasks'
-SLASH_DIR = 'tiles/2D Pixel Dungeon Asset Pack/items and trap_animation/Sword Slashes'
-ITEMS_DIR = 'tiles/2D Pixel Dungeon Asset Pack/items and trap_animation/RPG Items 16x16 Pack 1'
-KEYS_DIR = 'tiles/2D Pixel Dungeon Asset Pack/items and trap_animation/keys'
-SPRITE_SIZE = 16
-PLAYER_SPEED = 120  # [px/fps]
-PLAYER_SPEED /= FPS  # [px] - изменение координат за кадр
+from constants import *
 
 # Считываем координаты для анимированных декораций из json
 with open('maps/level1/elements_pos.json', 'r', encoding='utf8') as jsonf:
